1e3b8bb8320f051c9f2cf8f5274dc8fca46df4a1
Max De Marzi <maxdemarzi@hotmail.com>
2015-07-08 17:28:32 -0500

    hookup not quite working

---
+++ src/test/java/com/maxdemarzi/SocketKernelExtensionFactoryTest.java
@@ -0,0 +1,23 @@
+package com.maxdemarzi;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SocketKernelExtensionFactoryTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testNewKernelExtension() throws Exception {
+
+    }
+}
